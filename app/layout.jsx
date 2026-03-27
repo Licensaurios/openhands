a5fa371599889f93f62a5321524e130de654792b
luisDVSS <8elilustrador8@gmail.com>
2026-03-27 16:22:06 -0700

    changed: Sign in and Sign Up with clerk service, layouts.jsx and [[...rest]] pending: save user at back OH and clerk id

---
+++ app/layout.jsx
@@ -0,0 +1,37 @@
+// app/layout.jsx
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function RootLayout({ children }) {
+  //no usado mientras(no valo mdre para los disseños)
+  const clerkAparrence = {
+    variables: {
+      colorPrimary: "#0F1A2E",
+      colorBackground: "#ffffff",
+      colorText: "#111827",
+      colorInputBackground: "#f9fafb",
+      borderRadius: "6px",
+      fontFamily: "DM Sans, sans-serif",
+    },
+    elements: {
+      formButtonPrimary: {
+        backgroundColor: "#0F1A2E",
+        "&:hover": { backgroundColor: "#1e3a6e" },
+      },
+      headerTitle: {
+        fontFamily: "DM Serif Display, serif",
+        fontWeight: "400",
+      },
+      card: {
+        boxShadow: "none",
+        border: "0.5px solid #e5e7eb",
+      },
+    },
+  };
+  return (
+    <ClerkProvider>
+      <html lang="es">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
