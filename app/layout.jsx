12463f30ed5fc5c79d78100c22a5ee5927d5cf20
Jonathan <jonathanelian64@gmail.com>
2026-03-31 15:37:53 -0700

    fix: arreglar og image y agregar descripcion

--- app/layout.jsx
+++ app/layout.jsx
@@ -36,6 +36,7 @@ export default function RootLayout({ children }) {
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
             <link rel="manifest" href="/site.webmanifest"></link>
+            <meta property="og:description" content="OpenHands es una plataforma colaborativa de aprendizaje diseñada para estudiantes, jóvenes y educadores que desean compartir y descubrir materiales educativos de calidad" />
             <meta
                 property="og:image"
                 content="/og_openhands.png"
